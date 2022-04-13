205c79d58ca17d88de7bcccbec70bcfa64a9f630
Christopher Moore <christopher.e.moore@gmail.com>
2022-04-13 10:13:21 +0600

    first commit

---
+++ run.lua
@@ -0,0 +1,145 @@
+#!/usr/bin/env luajit
+--[[
+look at all combinations of N many multiplications
+
+lets start with N=2 for now
+
+then search through all max degree of a tensor
+--]]
+local table = require 'ext.table'
+local range = require 'ext.range'
+local symmath = require 'symmath'
+local var = symmath.var
+local Tensor = symmath.Tensor
+local tableToMul = symmath.tableToMul
+
+--symmath.tostring = symmath.export.SingleLine
+
+-- do i have that somewhere?
+local function multiter(start, finish)
+	local numvars = #start
+	assert(numvars == #finish)
+	return coroutine.wrap(function()
+		local vardegs = {}
+		for i=1,#start do
+			vardegs[i] = start[i]
+		end
+		local done
+		while true do
+			coroutine.yield(vardegs)
+			for i=2,numvars do
+				vardegs[i] = vardegs[i] + 1
+				if vardegs[i] <= finish[i] then
+					break
+				else
+					vardegs[i] = start[i]
+					if i == numvars then
+						done = true
+						break
+					end
+				end
+			end
+			if done then break end
+		end
+	end)
+end
+
+--[[
+aexpr = structure so far
+aindexes = options to set the symbol of bref[1+bi] to 
+bref = Tensor.Ref of var'b' to bind
+bi = index of bref that we are binding
+bmax = max of bi ... stop once it is past this
+--]]
+local function bind(terms, bref, bi, bmax, results)
+	if bi > bmax then return end
+	
+	bref = bref or Tensor.Ref(var'B')
+	local aexpr = tableToMul(terms)
+
+--print('expr: ' .. (aexpr * bref))
+	local fixed, summed, extra = (aexpr * bref):getIndexesUsed()
+	local symbols = fixed
+--print('fixed '..table.mapi(fixed, tostring):concat())
+--print('summed'..table.mapi(summed, tostring):concat())
+--print('extra '..table.mapi(extra, tostring):concat())
+
+	-- add an unused symbol to the end of 'symbols' *HERE*
+	local alreadyUsedSymbolKeys = {}
+	for _,t in ipairs{fixed, summed, extra} do
+		for _,index in ipairs(t) do 
+			alreadyUsedSymbolKeys[index.symbol] = true 
+		end
+	end
+--print('bi', bi)	
+	for i=2,bi do
+--print('removing old b symbol '..bref[i].symbol)		
+		alreadyUsedSymbolKeys[bref[i].symbol] = true
+	end
+	local newsymbol = select(2, Tensor.defaultSymbols:find(nil, function(sym)
+		return not alreadyUsedSymbolKeys[sym]
+	end))
+	symbols = symbols:filter(function(index)
+		return not table.find(table.sub(bref,2), nil, function(bi)
+			return bi.symbol == index.symbol
+		end)
+	end)
+--print('old expr symbols: '..table.mapi(symbols, tostring):concat()
+--	..' old bref symbols: '..table.sub(bref, 2):mapi(tostring):concat()
+--	..' new symbol '..newsymbol)	
+	
+	symbols:insert(Tensor.Index{symbol=newsymbol})
+
+--print("cycling thru "..table.mapi(symbols, tostring):concat' ')
+	for _,srci in ipairs(symbols) do
+--print('prev: '..require 'ext.tolua'(bref[bi].symbol or nil))
+--print('adding '..srci.symbol)
+		bref = bref:clone()
+		bref[1+bi] = Tensor.Index{
+			lower = false,
+			symbol = srci.symbol,
+		}
+
+		local prod = (aexpr * bref)
+
+		if bi == bmax then
+			-- TODO here instead of printing ... 
+			-- instead increment the "currentNumVars"
+			-- store aexpr * bref => aexpr
+			-- and process it again for a new bref
+
+			results:insert{expr=prod, degree=#prod:getIndexesUsed()}
+		elseif bi < bmax then
+			bind(terms, bref, bi+1, bmax, results)
+		end
+	end
+end
+
+for numvars=2,2 do
+	print('num vars = '..numvars)
+	for maxdegree=1,3 do
+		print('max degree = '..maxdegree)
+		for vardegs in multiter({maxdegree,1}, {maxdegree, maxdegree}) do
+			print('degree for each var:')
+			print(require'ext.tolua'(vardegs))
+			print()
+
+			-- expr = 1
+			-- fixed = expr:getIndexesUsed() = {}
+			-- then cycle through all possible index places of a
+			-- then for each index place, assign to one of the set of {all fixed symbols of expr, union a new symbol}
+			
+			local aexpr = var'A'( range(vardegs[1]):mapi(function(i) return ' _'..Tensor.defaultSymbols[i] end):concat() )
+			-- ok now for each index of 'b', cycle thru all 'a's indexes (or a new index)
+			
+			local results = table()
+			bind({aexpr}, nil, 1, vardegs[2], results)
+			results:sort(function(a,b) return a.degree < b.degree end)
+			for _,result in ipairs(results) do
+				print('expr: '..result.expr, 'degree: '..result.degree)
+		
+			end
+			print()
+		end
+	end
+end
