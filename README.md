a5e3aa93fdac47bc3752e8b7f1a23447665d2a85
Christopher Moore <christopher.e.moore@gmail.com>
2022-06-03 17:09:11 +0600

    added readme, made output methods modular

---
+++ README.md
@@ -0,0 +1,115 @@
+input degrees vs counts per degree
+
+|  |  |  |  | |0    |1    |2     |3    |4     |5    |6    |7   |8   |9  |10 |11 |12 |
+|--|--|--|--|-|-----|-----|------|-----|------|-----|-----|----|----|---|---|---|---|
+|1 |1 |  |  | |1    |     |1     |     |      |     |     |    |    |   |   |   |   |
+|2 |1 |  |  | |     |2    |      |1    |      |     |     |    |    |   |   |   |   |
+|2 |2 |  |  | |2    |     |4     |     |1     |     |     |    |    |   |   |   |   |
+|3 |1 |  |  | |     |     |3     |     |1     |     |     |    |    |   |   |   |   |
+|3 |2 |  |  | |     |6    |      |6    |      |1    |     |    |    |   |   |   |   |
+|3 |3 |  |  | |6    |     |18    |     |9     |     |1    |    |    |   |   |   |   |
+|1 |1 |1 |  | |     |3    |      |1    |      |     |     |    |    |   |   |   |   |
+|2 |1 |1 |  | |2    |     |5     |     |1     |     |     |    |    |   |   |   |   |
+|2 |2 |1 |  | |     |10   |      |8    |      |1    |     |    |    |   |   |   |   |
+|2 |2 |2 |  | |8    |     |30    |     |12    |     |1    |    |    |   |   |   |   |
+|3 |1 |1 |  | |     |6    |      |7    |      |1    |     |    |    |   |   |   |   |
+|3 |2 |1 |  | |6    |     |24    |     |11    |     |1    |    |    |   |   |   |   |
+|3 |3 |1 |  | |     |42   |      |54   |      |15   |     |1   |    |   |   |   |   |
+|3 |2 |2 |  | |     |48   |      |62   |      |16   |     |1   |    |   |   |   |   |
+|3 |3 |2 |  | |36   |     |186   |     |120   |     |21   |    |1   |   |   |   |   |
+|3 |3 |3 |  | |     |324  |      |558  |      |216  |     |27  |    |1  |   |   |   |
+|1 |1 |1 |1 | |3    |     |6     |     |1     |     |     |    |    |   |   |   |   |
+|2 |1 |1 |1 | |     |12   |      |9    |      |1    |     |    |    |   |   |   |   |
+|2 |2 |1 |1 | |10   |     |34    |     |13    |     |1    |    |    |   |   |   |   |
+|2 |2 |2 |1 | |     |68   |      |78   |      |18   |     |1   |    |   |   |   |   |
+|2 |2 |2 |2 | |60   |     |272   |     |156   |     |24   |    |1   |   |   |   |   |
+|3 |1 |1 |1 | |6    |     |27    |     |12    |     |1    |    |    |   |   |   |   |
+|3 |2 |1 |1 | |     |54   |      |68   |      |17   |     |1   |    |   |   |   |   |
+|3 |3 |1 |1 | |42   |     |204   |     |129   |     |22   |    |1   |   |   |   |   |
+|3 |2 |2 |1 | |48   |     |234   |     |142   |     |23   |    |1   |   |   |   |   |
+|3 |3 |2 |1 | |     |408  |      |666  |      |246  |     |29  |    |1  |   |   |   |
+|3 |3 |3 |1 | |324  |     |1998  |     |1638  |     |405  |    |36  |   |1  |   |   |
+|3 |2 |2 |2 | |     |468  |      |740  |      |264  |     |30  |    |1  |   |   |   |
+|3 |3 |2 |2 | |372  |     |2220  |     |1776  |     |428  |    |37  |   |1  |   |   |
+|3 |3 |3 |2 | |     |3996 |      |7992 |      |3852 |     |666 |    |45 |   |1  |   |
+|3 |3 |3 |3 | |3348 |     |23976 |     |23976 |     |7704 |    |999 |   |54 |   |1  |
+
+
+example: all tensor producs of up to 2 variables each with up to 3 degree:
+
+```text
+for degrees: {1, 1}
+A^a * B^a	degree: 0
+A^a * B^b	degree: 2
+
+for degrees: {2, 1}
+A^ab * B^a	degree: 1
+A^ab * B^b	degree: 1
+A^ab * B^c	degree: 3
+
+for degrees: {2, 2}
+A^ab * B^ab	degree: 0
+A^ab * B^ba	degree: 0
+A^ab * B^ca	degree: 2
+A^ab * B^cb	degree: 2
+A^ab * B^bc	degree: 2
+A^ab * B^ac	degree: 2
+A^ab * B^cd	degree: 4
+
+for degrees: {3, 1}
+A^abc * B^a	degree: 2
+A^abc * B^c	degree: 2
+A^abc * B^b	degree: 2
+A^abc * B^d	degree: 4
+
+for degrees: {3, 2}
+A^abc * B^ab	degree: 1
+A^abc * B^bc	degree: 1
+A^abc * B^cb	degree: 1
+A^abc * B^ba	degree: 1
+A^abc * B^ca	degree: 1
+A^abc * B^ac	degree: 1
+A^abc * B^bd	degree: 3
+A^abc * B^db	degree: 3
+A^abc * B^da	degree: 3
+A^abc * B^cd	degree: 3
+A^abc * B^dc	degree: 3
+A^abc * B^ad	degree: 3
+A^abc * B^de	degree: 5
+
+for degrees: {3, 3}
+A^abc * B^abc	degree: 0
+A^abc * B^bca	degree: 0
+A^abc * B^cab	degree: 0
+A^abc * B^bac	degree: 0
+A^abc * B^cba	degree: 0
+A^abc * B^acb	degree: 0
+A^abc * B^dac	degree: 2
+A^abc * B^adc	degree: 2
+A^abc * B^dab	degree: 2
+A^abc * B^adb	degree: 2
+A^abc * B^cda	degree: 2
+A^abc * B^dba	degree: 2
+A^abc * B^dca	degree: 2
+A^abc * B^dbc	degree: 2
+A^abc * B^dcb	degree: 2
+A^abc * B^cbd	degree: 2
+A^abc * B^cdb	degree: 2
+A^abc * B^cad	degree: 2
+A^abc * B^bda	degree: 2
+A^abc * B^bcd	degree: 2
+A^abc * B^acd	degree: 2
+A^abc * B^bdc	degree: 2
+A^abc * B^bad	degree: 2
+A^abc * B^abd	degree: 2
+A^abc * B^dce	degree: 4
+A^abc * B^dae	degree: 4
+A^abc * B^dea	degree: 4
+A^abc * B^deb	degree: 4
+A^abc * B^cde	degree: 4
+A^abc * B^ade	degree: 4
+A^abc * B^dec	degree: 4
+A^abc * B^dbe	degree: 4
+A^abc * B^bde	degree: 4
+A^abc * B^def	degree: 6
+```
--- run.lua
+++ run.lua
@@ -6,14 +6,21 @@ lets start with N=2 for now
 
 then search through all max degree of a tensor
 --]]
+local class = require 'ext.class'
 local table = require 'ext.table'
 local range = require 'ext.range'
 local symmath = require 'symmath'
+local tolua = require 'ext.tolua'
 local var = symmath.var
 local Tensor = symmath.Tensor
 local tableToMul = symmath.tableToMul
 
+local cmdline = require 'ext.cmdline'(...)
+
 --symmath.tostring = symmath.export.SingleLine
+
+local allmaxdegree = 3
+local maxnumvars = 4
 
 -- do i have that somewhere?
 local function multiter(start, finish)
@@ -27,7 +34,7 @@ local function multiter(start, finish)
 		local done
 		while true do
 			coroutine.yield(vardegs)
-			for i=2,numvars do
+			for i=1,numvars do
 				vardegs[i] = vardegs[i] + 1
 				if vardegs[i] <= finish[i] then
 					break
@@ -44,6 +51,58 @@ local function multiter(start, finish)
 	end)
 end
 
+-- same but only iterate over increasing tuples
+--[=[ i'm too dumb to get the ranges correct,...
+local function deciter(start, finish)
+	local numvars = #start
+	assert(numvars == #finish)
+	return coroutine.wrap(function()
+		local vardegs = {}
+		for i=1,#start do
+			vardegs[i] = start[i]
+		end
+		local done
+		while true do
+			coroutine.yield(vardegs)
+			for i=1,numvars do
+				vardegs[i] = vardegs[i] + 1
+				if vardegs[i] <= finish[i] then
+					break
+				else
+					if i == numvars then
+						done = true
+						break
+					end
+					vardegs[i] = start[i]
+					--[[
+					for j=i-1,1,-1 do
+						vardegs[j] = math.max(vardegs[j], vardegs[i])
+					end
+					vardegs[i+1] = vardegs[i+1]-1	-- undo the next inc
+					--]]
+				end
+			end
+			if done then break end
+		end
+	end)
+end
+--]=]
+-- [=[ ... so i'm just going to discard invalid iterations ... should go a bit slower than it could
+local function deciter(start, finish)
+	return coroutine.wrap(function()
+		for i in multiter(start, finish) do
+			local fail
+			for j=1,#i-1 do
+				if i[j] < i[j+1] then fail = true break end
+			end
+			if not fail then
+				coroutine.yield(i)
+			end
+		end
+	end)
+end
+--]=]
+
 local variableLetters = range(('AZ'):byte(1,2)):mapi(function(ch) return string.char(ch) end)
 
 --[[
@@ -51,31 +110,28 @@ aexpr = structure so far
 aindexes = options to set the symbol of bref[1+bi] to 
 bref = Tensor.Ref of var'b' to bind
 bi = index of bref that we are binding
-bmax = max of bi ... stop once it is past this
 --]]
-local function bind(terms, termMax, bref, bi, bmax, results, vardegs)
-	if bi > bmax then return end
+local function bind(terms, termMax, bref, bi, results, vardegs)
+	local aexpr = tableToMul(terms)
+	if #terms >= termMax then
+		results:insert{expr=aexpr, degree=#aexpr:getIndexesUsed()}
+		return
+	end
+
+	if bi > vardegs[#terms+1] then return end
 	
 	bref = bref or Tensor.Ref(var(variableLetters[#terms+1]))
-	local aexpr = tableToMul(terms)
 
---print('expr: ' .. (aexpr * bref))
 	local fixed, summed, extra = (aexpr * bref):getIndexesUsed()
 	local symbols = fixed
---print('fixed '..table.mapi(fixed, tostring):concat())
---print('summed'..table.mapi(summed, tostring):concat())
---print('extra '..table.mapi(extra, tostring):concat())
 
-	-- add an unused symbol to the end of 'symbols' *HERE*
 	local alreadyUsedSymbolKeys = {}
 	for _,t in ipairs{fixed, summed, extra} do
 		for _,index in ipairs(t) do 
 			alreadyUsedSymbolKeys[index.symbol] = true 
 		end
 	end
---print('bi', bi)	
 	for i=2,bi do
---print('removing old b symbol '..bref[i].symbol)		
 		alreadyUsedSymbolKeys[bref[i].symbol] = true
 	end
 	local newsymbol = select(2, Tensor.defaultSymbols:find(nil, function(sym)
@@ -86,58 +142,135 @@ local function bind(terms, termMax, bref, bi, bmax, results, vardegs)
 			return bi.symbol == index.symbol
 		end)
 	end)
---print('old expr symbols: '..table.mapi(symbols, tostring):concat()
---	..' old bref symbols: '..table.sub(bref, 2):mapi(tostring):concat()
---	..' new symbol '..newsymbol)	
 	
 	symbols:insert(Tensor.Index{symbol=newsymbol})
-	
---print("cycling thru "..table.mapi(symbols, tostring):concat' ')
 	for _,srci in ipairs(symbols) do
---print('prev: '..require 'ext.tolua'(bref[bi].symbol or nil))
---print('adding '..srci.symbol)
 		bref = bref:clone()
 		bref[1+bi] = Tensor.Index{
 			lower = false,
 			symbol = srci.symbol,
 		}
-
-		local prod = (aexpr * bref)
-
-		if bi == bmax then
-
-			local nextterms = table(terms)
-			nextterms:insert(bref)
-			if #nextterms >= termMax then 
-				results:insert{expr=prod, degree=#prod:getIndexesUsed()}
-			else
-				bind(nextterms, termMax, nil, 1, vardegs[#nextterms+1], results, vardegs)
-			end
-		elseif bi < bmax then
-			bind(table(terms), termMax, bref, bi+1, bmax, results, vardegs)
+		if bi < vardegs[#terms+1] then
+			-- if we still have more to recurse and handle the next index
+			bind(table(terms), termMax, bref, bi+1, results, vardegs)
+		elseif bi == vardegs[#terms+1] then
+			
+			-- if we just filled the last index then now we can produce a new var (if necessary)
+			-- TODO INSTEAD here, instead of making a new term here, instead redo this for all possible symmetries and antisymmetries
+			bind(table(terms):append{bref}, termMax, nil, 1, results, vardegs)
 		end
 	end
 end
 
--- TODO numvars==1 isn't working
-for numvars=2,3 do
-	print('num vars = '..numvars)
-	for maxdegree=1,3 do
-		print('max degree = '..maxdegree)
-		local startdeg = range(numvars):mapi(function(i) return i==1 and maxdegree or 1 end)
-		local enddeg = range(numvars):mapi(function(i) return maxdegree end)
-		for vardegs in multiter(startdeg, enddeg) do
-			print('degree for each var: '..require'ext.tolua'(vardegs))
-			print()
+local OutputAllExprs = class()
+function OutputAllExprs:output(vardegs, results, numResultsPerDegree)
+	-- #vardegs == maxdegree
+	io.write('for degrees: '..tolua(vardegs))
+	results:sort(function(a,b) return a.degree < b.degree end)
+	print()
+	for _,result in ipairs(results) do
+		print('expr: '..result.expr, 'degree: '..result.degree)
+	end
+	print()
+end
+
+local OutputCounts = class()
+function OutputCounts:output(vardegs, results, numResultsPerDegree)
+	-- #vardegs == maxdegree
+	io.write('for degrees: '..tolua(vardegs))
+	io.write(' counts per result degree:')
+	--[=[
+	print(' '..tolua(numResultsPerDegree))
+	--]=]
+	-- [=[
+	for _,k in ipairs(table.keys(numResultsPerDegree):sort()) do
+		io.write(' '..k..'->'..numResultsPerDegree[k])
+	end
+	print()
+end
+
+local OutputCountsMarkdown = class()
+function OutputCountsMarkdown:init()
+	-- can you do overflow in markdown tables?
+	-- if not then just print these here
+	self.t = table()
+	local row = table()
+	self.t:insert(row)	-- pad it with empty columsn in the end
+end
+function OutputCountsMarkdown:output(vardegs, results, numResultsPerDegree)
+	local row = table()
+	self.t:insert(row)
+	for i=1,maxnumvars do
+		row[i] = tostring(vardegs[i] or '')
+	end
+	row:insert''
+	
+	for k=0,(table.keys(numResultsPerDegree):sup()) do
+		local v = numResultsPerDegree[k]
+		row:insert(tostring(v or ''))
+	end
+end
+function OutputCountsMarkdown:done()
+	print'input degrees vs counts per degree'
+	print()		-- markdown needs a free line before the table to format it?
+	-- pad all rows to the max row
+	local numcols = self.t:mapi(function(row) return #row end):sup()
+	for i,row in ipairs(self.t) do
+		while #row < numcols do row:insert'' end
+	end
+	
+	-- now that we know the max degree, change the first row to have proper headerss
+	local firstrow = self.t[1]
+	for i=maxnumvars+2,#firstrow do
+		firstrow[i] = ''..(i-maxnumvars-2)
+	end
+
+	-- now for each column, count the num chars
+	local colsizes = table()
+	for j=1,numcols do
+		local colsize = self.t:mapi(function(row) return #row[j] end):sup()
+		for i,row in ipairs(self.t) do
+			row[j] = row[j]..(' '):rep(colsize - #row[j] + 1)
+		end
+	end
+	-- now print
+	for i,row in ipairs(self.t) do
+		local line = '|'..row:concat'|'..'|'
+		print(line)
+		if i == 1 then
+			print((line:gsub('[^|]', '-')))
+		end
+	end
+end
+
+local output = ({
+	exprs = OutputAllExprs,				-- print all expressions
+	count = OutputCounts,				-- just print the counts
+	markdown = OutputCountsMarkdown,	-- print counts in a markdown table
+})[cmdline.output or 'count']()
+
+for numvars=2,maxnumvars do
+	--print('num vars = '..numvars)
+	for maxdegree=1,allmaxdegree do
+		--print('max degree = '..maxdegree)
+		local startdeg = range(numvars-1):mapi(function(i) return 1 end)
+		local enddeg = range(numvars-1):mapi(function(i) return maxdegree end)
+		-- TODO make sure all unique permutations are iterated over
+		--for vardegs in multiter(startdeg, enddeg) do
+		for vardegs in deciter(startdeg, enddeg) do
+			vardegs = table(vardegs)
+			vardegs:insert(1, maxdegree)
 			
 			local terms = table()
 			local results = table()
-			bind(terms, numvars, nil, 1, vardegs[#terms+1], results, vardegs)
-			results:sort(function(a,b) return a.degree < b.degree end)
+			bind(terms, numvars, nil, 1, results, vardegs)
+			local numResultsPerDegree = {}
 			for _,result in ipairs(results) do
-				print('expr: '..result.expr, 'degree: '..result.degree)
+				numResultsPerDegree[result.degree] = (numResultsPerDegree[result.degree] or 0) + 1
 			end
-			print()
+			
+			output:output(vardegs, results, numResultsPerDegree)
 		end
 	end
 end
+if output.done then output:done() end
